654cbcb653a8b8fbf0c5a613fbdf5182882157ef
Alexander Artemenko <svetlyak.40wt@gmail.com>
2013-02-04 09:41:33 +0400

    Fixes get_version.

--- zokket/__init__.py
+++ zokket/__init__.py
@@ -7,7 +7,4 @@ from zokket.wsgi import WSGIServer
 VERSION = (1, 2, 0)
 
 def get_version():
-    version = '{}.{}'.format(VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '{}.{}'.format(version, VERSION[2])
-    return version
+    return '.'.join(map(str, filter(None, VERSION)))
